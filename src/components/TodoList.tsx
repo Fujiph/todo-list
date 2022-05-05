d10f06730535d9b163963466739d33c2acc74f6c
Fujiph <aueboonruanekamfu@gmail.com>
2022-05-05 20:06:37 +0700

    First commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+
+const TodoList:React.FC = () => {
+  return (
+    <div className='todos'>
+      {
+
+      }
+
+
+    </div>
+  )
+}
+
+export default TodoList
